5d9d1746f81fa24828a2ffa09d6a76890cfa9c4e
sirisenju <amrohore4real@gmail.com>
2026-06-07 16:45:13 +0100

    initial commit

---
+++ AI-Shipping/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-blue-600 underline p-6 bg-red-500 rounded-lg">
+        Tailwind works perfectly!
+      </h1>
+    </>
+  )
+}
+
+export default App
